c8a8bdb4f9679ac386e5ac06bf411edf837df28b
estanislao95 <alecaffa12@gmail.com>
2022-12-19 16:40:18 -0300

    Revert "ASD"
    This reverts commit 3fb675277010794bea306e9716b53f47df54f72d.

--- Assets/Scrips/Singletons/GameManager.cs
+++ Assets/Scrips/Singletons/GameManager.cs
@@ -6,12 +6,6 @@ public class GameManager : Singleton<GameManager>
 {
     public int Points;
     public Player player;
-<<<<<<< Updated upstream
-=======
-    public List<string> levels = new List<string>();
-    public int _remeber;
-
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -31,41 +25,5 @@ public class GameManager : Singleton<GameManager>
         UIManager.Instance.pointcounter(Points);
     }
 
-<<<<<<< Updated upstream
     
-=======
-    public void LoadLevel(int scene)
-    {
-        SceneManager.LoadScene(levels[scene]);
-    }
-
-    public void ToRandomScene()
-    {
-        SceneManager.LoadScene(SceneRandom());
-    }
-
-
-    public string SceneRandom()
-    {
-        int select = Random.Range(0, levels.Count);
-
-
-        while (select == _remeber)
-        {
-            select = Random.Range(0, levels.Count);
-        }
-
-        return levels[select];
-    }
-
-    public void PauseTime()
-    {
-        Time.timeScale = 0f;
-    }
-
-    public void ContinueTime()
-    {
-        Time.timeScale = 1f;
-    }
->>>>>>> Stashed changes
 }
